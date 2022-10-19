f1af303ab3b56a51de106e3fe7ef4401b9207237
dostiny <dogyeom2@gmail.com>
2022-10-19 21:45:32 +0900

    Update 11053_가장긴증가하는부분수열.py

--- 백준/11053_가장긴증가하는부분수열.py
+++ 백준/11053_가장긴증가하는부분수열.py
@@ -2,7 +2,7 @@ N = int(input())
 arr = list(map(int, input().split()))
 visited = [1 for i in range(N)]
 
-for i in range(N):
+for i in range(1, N):
     for j in range(i):
         if arr[i] > arr[j]:
             visited[i] = max(visited[i], visited[j] + 1)
